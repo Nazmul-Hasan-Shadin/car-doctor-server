d6a4866a46e054f56e415f241b1685cb903e37ef
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-10-30 21:25:11 +0600

    cookies and authenticatio n feature  added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express= require('express');
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser= require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express();
 const port= process.env.PORT || 5001;
 
 // meadleware 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser())
  
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.wjj4omp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process. env.DB_USER}:${process.env.DB_PASS}@cluster1.wjj4omp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +28,39 @@ const client = new MongoClient(uri, {
   }
 });
 
+          //  custom middlewear 
+const logger = async(req,res,next)=>{
+  console.log('called',req.host, req.originalUrl);
+  next()
+}
+const verifyToken=async(req,res,next)=>{
+  //  want to get token
+  const token = req.cookies?.token;
+  console.log('value of token in middleware first ',token);
+  if (!token) {
+     return res.status(401).send({message: 'Not authorized'})    
+  }
+  
+  jwt.verify(token,process.env.ACCESS_TOKEN,(error,decoded)=>{
+    // error 
+
+    if (error) {
+      console.log(error);
+      return res.status(401).send({message: 'unauthorized'})
+    }
+
+    // if token is valid then it would be decoded
+    console.log('value in the token ', decoded)
+    req.user=decoded
+    next()
+
+  })
+
+
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,14 +68,35 @@ async function run() {
 
   const serviceCollection= client.db('carDoctor').collection('services');
   const bookingCollection= client.db('carDoctor').collection('bookings')
-  
-    app.get('/services',async(req,res)=>{
+           
+  // auth related api
+
+  app.post('/jwt', logger,  async(req,res)=>{
+    const user= req.body;
+   
+    const token= jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+    console.log(token);
+    res.cookie('token',token,{
+      httpOnly:true,
+      secure:false,
+      
+
+    })
+    .send({success:true});
+  })
+    
+
+
+  //  services related api
+
+
+    app.get('/services' ,logger,async(req,res)=>{
         const cursor= await serviceCollection.find().toArray()
          res.send(cursor)
         
     })
 
-    app.get('/services/:id',async(req,res)=>{
+    app.get('/services/:id' ,async(req,res)=>{
       const id= req.params.id;
       const query={_id: new ObjectId(id)}
       const options= {
@@ -49,11 +109,12 @@ async function run() {
 
           //  bookings
 
- app.get('/bookings',async(req,res)=>{
-  console.log(req.query);
+ app.get('/bookings',logger,verifyToken,async(req,res)=>{
+  console.log(req.query.email);
+  
   let query= {};
   if (req.query?.email) {
-    query={email:req.query.email}
+    query={email:req.query?.email}
   }
   const cursor =await bookingCollection.find(query).toArray();
   res.send(cursor)
